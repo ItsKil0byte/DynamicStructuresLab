9c8a62cc7f1efa068a3c4e18b2026e33acf9b440
Alexander <itskilobyte@gmail.com>
2024-11-07 11:38:46 +0500

    Добавьте файлы проекта.

---
+++ DynamicStructuresLab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicStructuresLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ты думал, что здесь что-то будет?");
+        }
+    }
+}
