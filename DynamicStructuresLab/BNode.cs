b7a64aaa3fb196ee8263463d5af8e2f32d213d8c
sadtowa <toni.afanasev.0505@mail.ru>
2024-11-09 12:25:38 +0500

    BNode

--- DynamicStructuresLab/BNode.cs
+++ DynamicStructuresLab/BNode.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DynamicStructuresLab
 {
     public class BNode<T>
     {
-
-
-        public BNode(T data)
-        {
-
-        }
+        public BNode<T>? LeftNode { get; set; }
+        public BNode<T>? RightNode { get; set; }
+        public T? Data { get; set; }
     }
 }
