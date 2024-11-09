4fcec571cce5ae9dfa4260c466a57b3f04f02a94
sadtowa <toni.afanasev.0505@mail.ru>
2024-11-09 12:19:02 +0500

    binary node(for binary tree)

---
+++ DynamicStructuresLab/BNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicStructuresLab
+{
+    public class BNode<T>
+    {
+
+
+        public BNode(T data)
+        {
+
+        }
+    }
+}
